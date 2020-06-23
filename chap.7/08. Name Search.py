eab73f75b43ccbe9e69dd4ca937628ae85e08690
aokoro8215 <62453970+aokoro8215@users.noreply.github.com>
2020-06-23 19:42:14 -0400

    Update on No. 8 - Name sear exercise
    You need to create two .txt files containing a list of boy and girl names. eg(BoyNames.txt or GirlNames.txt)

--- chap.7/08. Name Search.py
+++ chap.7/08. Name Search.py
@@ -1,5 +1,76 @@
 # Chapter.7
-# 08. Name Search
-#
-# I couldn't get the source codes and text files.
+##No.8 Name search for popular name between 2000 and 2009.
+
+def readGirlNames(fileName):
+    girlsNamesFile = open(fileName, "r")
+    girlsNamesList = []
+
+    girlName = girlsNamesFile.readline()
+   
+    while girlName != "":
+        girlName = girlName.rstrip('\n')
+        girlsNamesList.append(girlName)
+        girlName = girlsNamesFile.readline()
+    
+
+    return girlsNamesList
+
+def readBoysNames(fileName):
+    boysNamesFile = open(fileName, "r")
+    boysNamesList = []
+
+    boyName = boysNamesFile.readline()
+
+    while boyName != "":
+        boyName = boyName.rstrip('\n')
+        boysNamesList.append(boyName)
+        boyName = boysNamesFile.readline()
+    
+
+    return boysNamesList
+
+def getusersearchnames():
+    usersearchname = input('Please enter the first name to search for:')
+    usersearchnamelist = []
+
+    while usersearchname != "-1":
+        usersearchnamelist.append(usersearchname)
+        usersearchname = input('Please enter next name to' +\
+                               'search for or -1 to continue:')
+    return usersearchnamelist
+
+def searchForName(usersearchnamelist, allNamesList):
+
+    foundnames =[]
+
+    for usersearchnamelistIndex in range(len(usersearchnamelist)):
+        if usersearchnamelist[usersearchnamelistIndex] in allNamesList:
+            foundnames.append(usersearchnamelist[usersearchnamelistIndex])
+
+    return foundnames
+
+
+def printserachresults(usersearchnamelist, foundnames):
+    for usersearchnamelistIndex in range(len(usersearchnamelist)):
+        if usersearchnamelist[usersearchnamelistIndex] in foundnames:
+            print(usersearchnamelist[usersearchnamelistIndex], 'was a popular name '\
+                  'between 2000 and 2009')
+        else:
+            print(usersearchnamelist[usersearchnamelistIndex], 'was not popular name '\
+                  'between 2000 and 2009')
+            
+
+def main():
+    girlsFileName = "GirlNames.txt"
+    boysFileName = "BoyNames.txt"
+
+    girlsNamesList = readGirlNames(girlsFileName)
+    boysNamesList = readBoysNames(boysFileName)
+    AllNamesList = girlsNamesList + boysNamesList
+    userSearchNamesList = getusersearchnames()
+    foundnamesList = searchForName(userSearchNamesList, AllNamesList)
+    printserachresults(userSearchNamesList, foundnamesList)
+main()
+
+
 
